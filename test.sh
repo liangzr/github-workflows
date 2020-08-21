7820541763e2d9d3e8c2353235edde6476618787
liangzr <liangzr@outlook.com>
2020-08-21 15:09:51 +0800

    check

--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
-
-full_tag="1.1.2"
-state=0
-
-check() {
-  if [ $? -ne 0 ]; then
-    echo "Check image failed"
-  fi
+ver=$RELEASE_VERSION
+check_pkg() {
+  curl -sL "http://registry.npmjs.com/@pipcook/$1" | jq -r '.versions."'$2'".version'
 }
 
-output=$(echo "1.1.2")
-[[ $state -eq 0 ]]
-check
-[[ "$output" == "$full_tag" ]]
-check
+check_image() {
+  curl -sL "https://hub.docker.com/v2/repositories/pipcook/pipcook/tags/$1" | jq -r '.name'
+}
+
+if
+  [ $(check_pkg pipcook-cli $ver) == $ver ] ||
+    [ $(check_pkg daemon $ver) == $ver ] ||
+    [ $(check_pkg pipboard $ver) == $ver ]
+then
+  echo ::set-env name=NPM_PUBLISHED::true
+fi
+
+if [ $(check_image $ver) == $ver ]; then
+  echo ::set-env name=DOCKER_PUBLISHED::true
+fi
